b54e0672369dfabcfa107086d50802d5e4826f3d
rwillems <mail@ronaldwillems.nl>
2022-12-10 12:00:16 +0100

    Day 10

--- src/main/kotlin/Day10/Day10.kt
+++ src/main/kotlin/Day10/Day10.kt
@@ -1,4 +1,101 @@
 package Day10
 
+import java.io.File
+
 object Day10 {
+    var instructions = mutableListOf<Pair<String,Int?>>()
+    var cycle = 0
+    var x = 1
+    var totalStrength = 0
+    var resultString = ""
+
+    fun readinput() {
+
+
+        File("src/main/resources/Day10/SampleInput.txt").forEachLine { line ->
+            val elements = line.split(" ")
+
+            if (elements[0]=="noop")
+                instructions.add(Pair(elements[0],null))
+            else instructions.add(Pair(elements[0],elements[1].toInt()))
+            //val elements2 = line.split( Pattern.compile("[,\\-]"))
+
+        }
+    }
+
+    fun getSignalStrengths():Int{
+        if (cycle in listOf<Int>(20,60,100, 140,180,220))  {
+          println(cycle* x)
+          return   cycle * x
+        }
+
+        return 0
+
+    }
+
+    fun part1() {
+        readinput()
+        instructions.forEach {
+            if (it.first == "noop") {
+                cycle++
+                totalStrength += getSignalStrengths()
+            }
+            else {
+                cycle++
+                totalStrength += getSignalStrengths()
+                cycle++
+                totalStrength += getSignalStrengths()
+                x+=it.second!!;
+
+            }
+
+
+
+        }
+        println(totalStrength)
+
+    }
+
+    fun drawPixle (){
+        var xpos=x.mod(40)
+        var cyclepos = cycle.mod(40)
+
+        if (xpos in cyclepos-2 .. cyclepos )
+            resultString+="#"
+        else
+            resultString+="."
+
+    }
+
+    fun part2(){
+
+        readinput()
+        instructions.forEach {
+            if (it.first == "noop") {
+                cycle++
+                drawPixle()
+            }
+            else {
+                cycle++
+                drawPixle()
+
+                cycle++
+                drawPixle()
+                x+=it.second!!;
+
+            }
+
+
+
+        }
+
+        //Print Result
+        for (i in 1..6) {
+            println(resultString.substring((i-1)*40..(40*i)-1))
+
+        }
+
+
+
+    }
 }
---
+++ src/test/kotlin/Day10/Day10Test.kt
@@ -0,0 +1,18 @@
+package Day10
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class Day10Test {
+
+    @Test
+    fun part1() {
+        Day10.part1()
+    }
+
+    @Test
+    fun part2() {
+        Day10.part2()
+    }
+}
